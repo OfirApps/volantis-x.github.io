16f3122ce75e11a978b780546b0d60d2cf902b93
Ofir Shtorch <ijumpitsus@gmail.com>
2023-12-31 20:09:16 +0200

    yuh

--- themes/volantis/scripts/tags/o-matkondiv.js
+++ themes/volantis/scripts/tags/o-matkondiv.js
@@ -4,13 +4,13 @@ hexo.extend.tag.register('matkoning', function() {
     return `<style>
     .matkoning ul li {list-style-position: inside;list-style-type: disclosure-closed;}
     .md ol>li {list-style-position: inside;}
-    </style><div class="matkoning">`;
+    </style><box class="matkoning">`;
 });
 
 hexo.extend.tag.register('divend', function() {
-    return `</div>`;
+    return `</box>`;
 });
 
 hexo.extend.tag.register('taavon', function() {
-    return `<div style="text-align: center;font-size: large;"><b>בתאבון!</b></div>`;
+    return `<box style="text-align: center;font-size: large;"><b>בתאבון!</b></box>`;
 });
