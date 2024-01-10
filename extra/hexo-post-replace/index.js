c90a24106dfb7adf9534f83482c0b226e3dc451c
Ofir Shtorch <ijumpitsus@gmail.com>
2024-01-10 11:59:03 +0200

    yuh

---
+++ extra/hexo-post-replace/index.js
@@ -0,0 +1,20 @@
+
+const config = hexo.config.post_replace;
+
+hexo.extend.filter.register('before_post_render', function(data){
+  if (config.before) {
+    for (const key in config.before) {
+      data.content = data.content.replaceAll(key, config.before[key]);
+    }
+  }
+  return data;
+});
+
+hexo.extend.filter.register('after_post_render', function(data){
+  if (config.after) {
+    for (const key in config.after) {
+      data.content = data.content.replaceAll(key, config.after[key]);
+    }
+  }
+  return data;
+});
