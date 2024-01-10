827f3e820fdfe982dceaeb0e9e045b6e5517ce6f
Ofir Shtorch <ijumpitsus@gmail.com>
2024-01-10 12:15:47 +0200

    yuh

--- extra/hexo-post-replace/index.js
+++ extra/hexo-post-replace/index.js
@@ -1,7 +1,7 @@
 
 const config = hexo.config.post_replace;
 
-hexo.extend.filter.register('before_post_render', function(data){
+hexo.extend.filter.register('before_render:html', function(data){
   if (config.before) {
     for (const key in config.before) {
       data.content = data.content.replaceAll(key, config.before[key]);
@@ -10,7 +10,7 @@ hexo.extend.filter.register('before_post_render', function(data){
   return data;
 });
 
-hexo.extend.filter.register('after_post_render', function(data){
+hexo.extend.filter.register('after_render:html', function(data){
   if (config.after) {
     for (const key in config.after) {
       data.content = data.content.replaceAll(key, config.after[key]);
